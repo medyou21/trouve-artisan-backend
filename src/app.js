07236413c852ffa81a6d7a9b905711972271b61f
medyou21 <92979555+medyou21@users.noreply.github.com>
2026-01-12 17:04:58 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -42,7 +42,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.get('/', (req, res) => {
+  res.json({ status: 'API OK 🚀' });
+});
 app.use("/api/artisans", artisanRoutes);
 app.use("/api/categories", categoryRoutes);
 
