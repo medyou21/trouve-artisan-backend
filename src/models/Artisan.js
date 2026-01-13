f86b6d3fc59d35a8e2b7448ad40895ca027f63e5
mohamed <mohamed.ing2@gmail.com>
2026-01-13 19:47:04 +0100

    fix: artisan.js

--- src/models/Artisan.js
+++ src/models/Artisan.js
@@ -1,5 +1,6 @@
-const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
+const { DataTypes } = require("sequelize");
+
 
 const Artisan = sequelize.define(
   "Artisan",
