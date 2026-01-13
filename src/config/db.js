04c842d8921815cb9760b9a2d96cfd8da1a85887
mohamed <mohamed.ing2@gmail.com>
2026-01-13 20:34:37 +0100

    fix 2: src/config/db.js

--- src/config/db.js
+++ src/config/db.js
@@ -20,7 +20,7 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.MYSQL_ADDON_DB, process.env.MYSQL_ADDON_USER, process.env.MYSQL_ADDON_PASSWORD, {
   host: process.env.MYSQL_ADDON_HOST,
   port: process.env.MYSQL_ADDON_PORT,
-  dialect: "mariadb",
+  dialect: "mysql",
   logging: false,
 });
 
