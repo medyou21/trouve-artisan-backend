5cf7e28243ac455a6474b05bd2201873c18dea94
mohamed <mohamed.ing2@gmail.com>
2026-01-13 13:17:05 +0100

    mise à jour server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.listen(PORT, () => {
 });
  */
 
-require("dotenv").config();
+/* require("dotenv").config();
 const app = require("./src/app");
 const sequelize = require("./src/config/db");
 
@@ -24,7 +24,31 @@ sequelize.authenticate()
   .then(() => {
     console.log("✅ MariaDB connecté");
     app.listen(PORT, () =>
-      console.log(`🚀 API sur http://localhost:${PORT}`)
+      console.log(`🚀 API sur http://0.0.0.0:${PORT}`)
     );
   })
   .catch((err) => console.error("❌ Erreur DB", err));
+ */
+
+
+  require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const { connectDB } = require("./src/config/db");
+
+const app = express();
+app.use(cors({
+  origin: "https://trouve-artisan-frontend.vercel.app" // l’URL de frontend
+}));
+app.use(express.json());
+
+
+
+// Connectez la DB puis démarrez le serveur
+connectDB().finally(() => {
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server listening on 0.0.0.0:${PORT}`);
+  });
+});
